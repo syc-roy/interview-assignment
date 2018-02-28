79aefa37e88e0ca410430a4c1d0c346de691c8f8
XingSong Lin <linxingsong@gmail.com>
2018-02-28 18:34:07 -0500

    setup router template

--- server/routes.js
+++ server/routes.js
@@ -8,8 +8,36 @@ const food = require('./model/food/router');
 router.use('/api/user', user);
 router.use('/api/food', food);
 
-router.get('*', function(req, res) {
-  res.sendfile(path.resolve('client/dist/index.html'));
+//router.get('*', function(req, res) {
+//  res.sendfile(path.resolve('client/dist/index.html'));
+//});
+
+router.get('/', function(req, res){
+  res.render(path.resolve('client/dist/index.ejs')); //loading the index.ejs file
 });
+
+router.get('/login', function(req, res){
+  res.render(path.resolve('client/dist/login.ejs'));
+});
+
+router.get('/signup', function(req, res){
+  res.render(path.resolve('client/dist/signup.ejs'));
+});
+
+//new to add auth later
+router.get('/profile', isLoggedIn, function(req, res){
+  res.render(path.resolve('client/dist/profile.ejs'));
+});
+
+router.get('/logout', function(req,res){
+  req.logout();
+  res.redirect('/');
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated())
+    return next();
+  res.redirect('/');
+}
 
 module.exports = router;
